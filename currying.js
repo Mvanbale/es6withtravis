a35a79462d20010cb7dafbb35aca236a5c4fe8d1
Michael van Bale <mvanbale@gmail.com>
2016-03-23 14:50:13 +0100

    extracted functionality and composed them into one

--- currying.js
+++ currying.js
@@ -1,30 +1,45 @@
 var logger = console.log;
-var User =
-  name =>
-    descriptor =>
-      food => {
-        logger(`${name} is an ${descriptor} person, he likes to eat ${food}.`);
-      };
-var billy = User('Billy')('average')('cheese');
+const R = require('ramda');
 
 var ingredients = [{ingredient: 'kaas', amount: 12, unit: 'gram'},
 {ingredient: 'ham', amount: 100, unit: 'gram'},
  {ingredient: 'brood', amount: 4, unit: 'gram'}];
 
+
+
+const filterOnIngredient = (data) => {
+  if (data.ingredient !== null) data.input = data.input.filter(entry => entry.ingredient.toLowerCase() === data.ingredient.toLowerCase());
+  return data;
+};
+
+const filterOnAmountLower = (data) => {
+  if (data.amountLower !== null) data.input = data.input.filter(entry => entry.amount >= data.amountLower);
+  return data;
+};
+
+const filterOnAmountUpper = (data) => {
+  if (data.amountUpper !== null) data.input = data.input.filter(entry => entry.amount <= data.amountUpper);
+  return data;
+};
+
+const filterOnUnit = (data) => {
+  if (data.unit !== null) data.input = data.input.filter(entry => entry.unit.toLowerCase() === data.unit.toLowerCase());
+  return data;
+};
+
+const combinedFilter = R.pipe(filterOnIngredient, filterOnAmountLower, filterOnAmountUpper, filterOnUnit);
+
 const filter =
   input =>
-    (ingredient = null) =>
+    (ingredient) =>
       amountLower =>
         amountUpper =>
           unit => {
-            if (ingredient !== null) input = input.filter(entry => entry.ingredient.toLowerCase() === ingredient.toLowerCase());
-            if (amountLower !== null) input = input.filter(entry => entry.amount >= amountLower);
-            if (amountUpper !== null) input = input.filter(entry => entry.amount <= amountUpper);
-            if (unit !== null) input = input.filter(entry => entry.unit.toLowerCase() === unit.toLowerCase());
-            return input;
+            var data = {input, ingredient, amountLower, amountUpper, unit};
+            return combinedFilter(data).input;
           };
-const myFilteredIngredients = filter(ingredients)('hAm')(0)(null)('gram');
 
+const myFilteredIngredients = filter(ingredients)(null)(0)(null)('gram');
 console.log(myFilteredIngredients);
 
 //result:z
