93fe9853fdfa0b2208c52159bff6c301079ef9fa
Michael van Bale <mvanbale@gmail.com>
2016-03-23 13:50:21 +0100

    Made a filter with currying, now to make the internal logic into one composed function

--- currying.js
+++ currying.js
@@ -7,6 +7,25 @@ var User =
       };
 var billy = User('Billy')('average')('cheese');
 
+var ingredients = [{ingredient: 'kaas', amount: 12, unit: 'gram'},
+{ingredient: 'ham', amount: 100, unit: 'gram'},
+ {ingredient: 'brood', amount: 4, unit: 'gram'}];
 
-//result:
+const filter =
+  input =>
+    ingredient =>
+      amountLower =>
+        amountUpper =>
+          unit => {
+            if (ingredient !== null) input = input.filter((entry) => entry.ingredient.toLowerCase() === ingredient.toLowerCase());
+            if (amountLower !== null) input = input.filter((entry) => entry.amount >= amountLower);
+            if (amountUpper !== null) input = input.filter((entry) => entry.amount <= amountUpper);
+            if (unit !== null) input = input.filter((entry) => entry.unit.toLowerCase() === unit.toLowerCase());
+            return input;
+          };
+const myFilteredIngredients = filter(ingredients)(null)(0)(12)('gram');
+
+console.log(myFilteredIngredients);
+
+//result:z
 //  Billy is an average person, he likes to eat cheese.
